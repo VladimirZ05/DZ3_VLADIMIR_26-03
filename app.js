ec5981d820a98a291ddd7bd34656568f6da94aa0
VladimirZ05 <132680480+VladimirZ05@users.noreply.github.com>
2023-05-12 22:00:42 +0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,32 +1,37 @@
-// При помощи цикла for напишите программу «FizzBuzz», которая будет выводить в консоли все числа от 1 до 100 с тремя исключениями:
-//  ⁃ 1)Если число делится нацело на 3, вместо числа должно выводиться «Fizz»
-//  ⁃ 2)Если число нацело делится на 5 (но не на 3), вместо числа должно выводиться «Buzz»
-//  ⁃ 3)После этого сделайте так, чтобы программа выводила «FizzBuzz» для чисел, которые делятся и на 3, и на 5
-//
-// ДЗ2:
-//  ⁃ Есть массив с элементами: ['milk', 'beer', 'beer', 'milk', 'milk']. Необходимо запустить в цикле проверку для каждого элемента массива -
-// когда встречается элемент со значением "milk", в консоль выводить строку "good", если встречается элемент со значением "beer",
-//     выводить в консоль строку "bad". Выполнить дз при помощи цикла.
-
-
-var FizzBuzz
-for (var i = 1 ; i<101; i++ ){
-if (i%3===0) console.log('Fizz');
-else if (i%5===0) console.log('Buzz');
-else if (i%3===0 && i%5===0) console.log('FizzBuzz')
-
-    continue
+var StarBucks = {
+    coffee: {
+        Latte: 'Latte Coffee',
+        Cappuccino: 'Cappuccino Coffee',
+        Mokiato: 'Mokiato Coffee',
+        Espresso: 'Espresso Coffee'
+    },
+    tea: {
+        Black: 'Black Tea',
+        Green: 'Green Tea',
+        Herbal: 'Herbal Tea',
+        Fruit: 'Fruit Tea',
+    }
 }
+console.log(StarBucks)
+console.log(StarBucks.coffee)
+console.log(StarBucks.coffee.latte)
+console.log(StarBucks.coffee.cappuccino)
+console.log(StarBucks.coffee.mokiato)
+console.log(StarBucks.coffee.espresso)
 
+console.log(StarBucks.tea)
+console.log(StarBucks.tea.black)
+console.log(StarBucks.tea.green)
+console.log(StarBucks.tea.herbal)
+console.log(StarBucks.tea.fruit)
 
-
-var products = ['milk', 'beer', 'beer', 'milk', 'milk']
-var bad = ['beer']
-var good = ['milk']
-for (var i = 0; i<products.length; i++){
-if (bad.includes(products[i].toLowerCase())){
-    console.log('Bad!')}
-if (good.includes(products[i].toLowerCase()))
-    console.log('Good!')
-    continue
+var Order = prompt('Enter your order')
+if (Order === 'Latte' || Order === 'Cappuccino' || Order === 'Mokiato' || Order === 'Espresso'){
+    console.log('Your Coffee order was successfully done, please wait till it ready.')
 }
+else if (Order === 'Black tea' || Order === 'Green tea' || Order === 'Herbal tea' || Order === 'Fruit tea'){
+    console.log('Your Tea order was successfully done, please wait till it ready.')
+}
+else
+    console.log('Your order is not correct or its not avialable now. Please try back later!')
+
